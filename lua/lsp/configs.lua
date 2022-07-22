1ab126b6f7fb3aab92f761c098a6cf7962fec61b
Allen Chang <allen.ty.chang@gmail.com>
2022-07-22 14:48:22 +0800

    Add pyright

--- lua/lsp/configs.lua
+++ lua/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "rust_analyzer", "bashls", "yamlls" }
+local servers = { "jsonls", "sumneko_lua", "rust_analyzer", "bashls", "yamlls", "pyright" }
 
 lsp_installer.setup({
   ensure_installed = servers,
