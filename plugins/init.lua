16b4bc672571ebbcf7341cf45e2966ac61eb9234
Allen Chang <allen.ty.chang@gmail.com>
2022-10-21 23:33:21 -0400

    add noice.nvim

--- plugins/init.lua
+++ plugins/init.lua
@@ -40,4 +40,12 @@ return {
       require("dapui").setup()
     end,
   },
+
+  ["folke/noice.nvim"] = {
+    requires = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+    event = "VimEnter",
+    config = function()
+      require("noice").setup()
+    end,
+  },
 }
